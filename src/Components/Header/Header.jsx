18deb98b674bb0a4390e5d5e52b592ea6b5c691e
YakovchukIvan <litvinovjobs@gmail.com>
2023-05-03 21:08:22 +0300

    working react project

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,30 +4,30 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return(
         <>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
 
                 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
                         <Link to='/new-contact'>
                             Add contact
                         </Link>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <Link to='/'>
                             Contact List
                         </Link>
                     </li>
                                     
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                <form className="d-flex" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-primary" type="submit">Search</button>
                 </form>
                 </div>
             </div>
--- src/Pages/NewContact/NewContact.jsx
+++ src/Pages/NewContact/NewContact.jsx
@@ -1,8 +1,108 @@
-import './NewContact.css'
+import './NewContact.scss'
+
+import * as Yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import {v4 as uuidv4} from 'uuid'
 
 const NewContact = () => {
+    const initialValues = {
+        id: uuidv4(),
+        name: '',
+        phone: '',
+        email: '',
+        avatar: '',
+        gender: '',
+        status: '',
+        favorite: ''
+    }
+
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        phone: Yup.string().required('Phone is required'),
+        email: Yup.string().email('invalid email').required('Email is required'),
+        avatar: Yup.string().url('Invalid URL').required('Avatar is required'),
+        gender: Yup.string().oneOf(['Men', 'Women'],'Invalid gender').required('Gender is required'),
+        status: Yup.string().oneOf(['Work', 'Family', 'Private', 'Friends'],'Invalid status').required('Status is required'),
+        favorite: Yup.boolean()
+    })
+
+    const handleSubmit = (values) => {
+        console.log(values);
+    }
+
+
     return (
-        <p>NewContact</p>
+        <>
+        <h2>New Contact</h2>
+            <div className="registrationForm">
+
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+                    {({isSubmitting}) => (
+                        <Form>
+                            <div>
+                                <label htmlFor='name'>Name</label>
+                                <Field type='text' name='name'/>
+                                <ErrorMessage name='name' />
+                            </div>
+
+                            <div>
+                                <label htmlFor='phone'>Phone</label>
+                                <Field type='text' name='phone' />
+                                <ErrorMessage name='phone' />
+                            </div>
+
+                            <div>
+                                <label htmlFor='email'>Email</label>
+                                <Field type='email' name='email' />
+                                <ErrorMessage name='email' />
+                            </div>
+
+                            <div>
+                                <label htmlFor='avatar'>Avatar</label>
+                                <Field type='text' name='avatar' />
+                                <ErrorMessage name='avatar' />
+                            </div>
+
+                            <div>
+                                <label htmlFor='gender'>Gender</label>
+                                <Field as='select' name='gender'>
+                                    <option value="">Choose gender</option>
+                                    <option value="Men">Men</option>  
+                                    <option value="Women">Women</option>    
+                                </Field>
+                                <ErrorMessage name='gender' />
+                            </div>
+
+                            <div>
+                                <label htmlFor='status'>Status</label>
+                                <Field as='select' name='status'>
+                                    <option value="">Choose status</option>
+                                    <option value="Work">Work</option>  
+                                    <option value="Family">Family</option>
+                                    <option value="Private">Private</option>    
+                                    <option value="Friends">Friends</option>        
+                                </Field>
+                                <ErrorMessage name='status' />
+                            </div>
+
+                            <div>
+                                <label htmlFor='favorite'>Favorite</label>
+                                <Field type='checkbox' name='favorite' />
+                            </div>
+                            <button type='submit' className="btn btn-primary btn-lg" disabled={isSubmitting}>Add</button>
+                        </Form>
+                        )           
+                    }
+                </Formik>
+
+                
+
+
+
+
+
+            </div>
+        </>
     )
 }
 
